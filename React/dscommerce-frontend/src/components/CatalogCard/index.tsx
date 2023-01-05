27ee83d409c4efb3d428c8207a6ec663afb39db2
Pollyana <pollyana.oliveira@gmail.com>
2023-01-05 19:54:14 -0300

    Button Next Page

---
+++ React/dscommerce-frontend/src/components/CatalogCard/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import computerImg from '../../assets/computer.png';
+
+export default function CatalogCard() {
+  return (
+    <div className="dsc-card">
+      <div className="dsc-catalog-card-top dsc-line-bottom">
+        <img src={computerImg} alt="Computer" />
+      </div>
+      <div className="dsc-catalog-card-bottom">
+        <h3>R$ 5000,00</h3>
+        <h4>Computador Gamer XT</h4>
+      </div>
+    </div>
+  );
+}
