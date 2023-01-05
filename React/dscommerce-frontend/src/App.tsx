37ce50ba2a2b8d7f8b6676e4594ebc682e0d2f53
Pollyana <pollyana.oliveira@gmail.com>
2023-01-05 17:16:16 -0300

    Header Client

--- React/dscommerce-frontend/src/App.tsx
+++ React/dscommerce-frontend/src/App.tsx
@@ -1,64 +1,52 @@
-import ComputerImg from './assets/computer.png';
-import cartImg from './assets/cart.svg';
 import './App.css'
+import ComputerImg from './assets/computer.png';
+import HeaderClient from './components/HeaderClient';
 
-function App() {
+export default function App() {
 
   return (
   <>
-     <header className="dsc-header-client">
-      <nav className="dsc-container">
-        <h1>DSCommerce</h1>
-        <div className="dsc-navbar-right">
-          <div className="dsc-menu-items-container">
-            <div className="dsc-menu-item">
-              <img src={cartImg} alt="Carrinho de compras" />
+    <HeaderClient /> 
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <div className="dsc-card dsc-mb20">
+            <div className="dsc-product-details-top dsc-line-bottom">
+              <img src={ComputerImg} alt="Computador" />
+            </div>
+            <div className="dsc-product-details-bottom">
+              <h3>R$ 5000,00</h3>
+              <h4>Computador Gamer XT</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <div className="dsc-category-container">
+                  <div className="dsc-category">
+                      Eletrônicos
+                  </div>
+                  <div className="dsc-category">
+                      Computadores
+                  </div>
+              </div>
             </div>
           </div>
-          <a href="#">Entrar</a>
-        </div>
-      </nav>
-    </header>
-    <main>
-      <section id="product-details-section" className="dsc-container">
-        <div className="dsc-card dsc-mb20">
-          <div className="dsc-product-details-top dsc-line-bottom">
-            <img src={ComputerImg} alt="Computador" />
+          <div className="dsc-btn-page-container">
+              <div className="dsc-btn dsc-btn-blue">
+                  Comprar
+              </div>
+              <div className="dsc-btn dsc-btn-white">
+                  Início
+              </div>
           </div>
-          <div className="dsc-product-details-bottom">
-            <h3>R$ 5000,00</h3>
-            <h4>Computador Gamer XT</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <div className="dsc-category-container">
-                <div className="dsc-category">
-                    Eletrônicos
-                </div>
-                <div className="dsc-category">
-                    Computadores
-                </div>
-            </div>
-          </div>
-        </div>
-        <div className="dsc-btn-page-container">
-            <div className="dsc-btn dsc-btn-blue">
-                Comprar
-            </div>
-            <div className="dsc-btn dsc-btn-white">
-                Início
-            </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
   </>
   );
 }
 
-export default App
+
