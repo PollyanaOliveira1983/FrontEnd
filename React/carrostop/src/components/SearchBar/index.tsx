b60c53748f7831bcb13b14ff33165a603b24ff0b
Pollyana <pollyana.oliveira@gmail.com>
2023-01-06 15:24:12 -0300

    SearchBar

---
+++ React/carrostop/src/components/SearchBar/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export default function SearchBar() {
+  return (
+    <form className="ct-search-bar">
+      <input className="ct-form-control" type="text" placeholder="Digite sua busca" />
+      <button className="ct-search-bar-submit" type="submit">Buscar</button>
+    </form>
+  )
+}
--- React/carrostop/src/routes/Catalog/index.tsx
+++ React/carrostop/src/routes/Catalog/index.tsx
@@ -1,9 +1,24 @@
+import './styles.css';
 import Header from "../../components/Header";
+import SearchBar from '../../components/SearchBar';
 
 export default function Catalog(){
 
     return (
-        <Header />        
+        <>
+            <Header />
+            <main className="ct-catalog-main">
+                <section id="ct-catalog-section" className="ct-container">
+                    <div className="ct-search-bar-container">
+                        <SearchBar />
+                    </div>
+                    <div className="ct-catalog-cards-container">
+                        <div>teste</div>
+                    </div>
+                </section>
+            </main>
+        </>
+              
     );
 
 }
