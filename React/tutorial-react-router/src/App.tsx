6342bde508f6555eb157970033f57303e1e3a9b0
Pollyana <pollyana.oliveira@gmail.com>
2023-01-13 11:45:10 -0300

    Adding routes

--- React/tutorial-react-router/src/App.tsx
+++ React/tutorial-react-router/src/App.tsx
@@ -1,19 +1,18 @@
-import { Link } from 'react-router-dom'
-import './App.css'
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  
   return (
     <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
     >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
-      </nav>
-  )
+      <Link to="/invoices">Invoices</Link> |{" "}
+      <Link to="/expenses">Expenses</Link>
+    </nav>
+  );
 }
 
-export default App
+export default App;
