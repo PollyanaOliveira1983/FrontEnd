f9a455e88cbb7ad05445f4886c61b031d7b98488
Pollyana <pollyana.oliveira@gmail.com>
2023-01-13 11:30:49 -0300

    Links without full page reload

--- React/tutorial-react-router/src/App.tsx
+++ React/tutorial-react-router/src/App.tsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
   
   return (
-    <div>
-      <h1>Bookkeeper!</h1>
-    </div>
+    <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+    >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
   )
 }
 
