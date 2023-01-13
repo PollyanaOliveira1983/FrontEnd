84bc59b7a32298edd9ea18855a32d35233a76da7
Pollyana <pollyana.oliveira@gmail.com>
2023-01-13 13:30:21 -0300

    Rotas aninhadas(sub rotas)

--- React/tutorial-react-router/src/App.tsx
+++ React/tutorial-react-router/src/App.tsx
@@ -1,17 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <nav
-      style={{
-        borderBottom: "solid 1px",
-        paddingBottom: "1rem",
-      }}
-    >
-      <Link to="/invoices">Invoices</Link> |{" "}
-      <Link to="/expenses">Expenses</Link>
-    </nav>
+    <>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
