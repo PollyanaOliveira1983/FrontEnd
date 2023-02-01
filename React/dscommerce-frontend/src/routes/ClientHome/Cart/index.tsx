172c6b6aa2b1e6fe7e15cac6e21f7c21ac2402f7
Pollyana <pollyana.oliveira@gmail.com>
2023-02-01 13:33:39 -0300

    Mensagem de carrinho vazio

--- React/dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
+++ React/dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
@@ -1,16 +1,8 @@
 import "./styles.css";
 import * as cartService from "../../../services/cart-service";
 import { useState } from "react";
-import { OrderDTO, OrderItemDTO } from "../../../models/order";
+import { OrderDTO } from "../../../models/order";
 
-
-const item1 : OrderItemDTO = new OrderItemDTO(
-    4, 1, "PC Gamer", 1200, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
-)
-
-const item2 :OrderItemDTO =new OrderItemDTO(
-    5, 2, "Rails for Dummies", 100.99, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
-) 
 
 export default function Cart() {
     
@@ -21,34 +13,46 @@ export default function Cart() {
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
-        <div className="dsc-card dsc-mb20">
-          {
-            cart.items.map((item) => (
-                <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
-                    <div className="dsc-cart-item-left">
-                        <img src={item.imgUrl} alt={item.name} />
-                        <div className="dsc-cart-item-description">
-                            <h3>{item.name}</h3>
-                            <div className="dsc-cart-item-quantity-container">
-                                <div className="dsc-cart-item-quantity-btn">
-                                    -
-                                </div>
-                                <p>{item.quantity}</p>
-                                <div className="dsc-cart-item-quantity-btn">
-                                    +
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="dsc-cart-item-right">
-                        R$ {(item.price * item.quantity).toFixed(2)}
-                    </div>
+        {
+          cart.items.length === 0
+          ? (
+            <div>
+              <h2 className="dsc-section-title dsc-mb20">Seu carrinho está vazio</h2>
+            </div>
+          )
+          : (
+              <div className="dsc-card dsc-mb20">
+                {
+                  cart.items.map((item) => (
+                      <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
+                          <div className="dsc-cart-item-left">
+                              <img src={item.imgUrl} alt={item.name} />
+                              <div className="dsc-cart-item-description">
+                                  <h3>{item.name}</h3>
+                                  <div className="dsc-cart-item-quantity-container">
+                                      <div className="dsc-cart-item-quantity-btn">
+                                          -
+                                      </div>
+                                      <p>{item.quantity}</p>
+                                      <div className="dsc-cart-item-quantity-btn">
+                                          +
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="dsc-cart-item-right">
+                              R$ {item.subTotal.toFixed(2)}
+                          </div>
+                      </div>
+                ))}
+                <div className="dsc-cart-total-container">
+                  <h3>R$ {cart.total.toFixed(2)}</h3>
                 </div>
-          ))}
-          <div className="dsc-cart-total-container">
-            <h3>R$ 15000,00</h3>
-          </div>
-        </div>
+              </div>
+          )
+        }
+
+        
 
         <div className="dsc-btn-page-container">
           <div className="dsc-btn dsc-btn-blue">Finalizar pedido</div>
