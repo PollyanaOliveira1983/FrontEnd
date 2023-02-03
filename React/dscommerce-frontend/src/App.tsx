22d61b5a28dfab0a81bbe28ceb746880a794aa17
Pollyana <pollyana.oliveira@gmail.com>
2023-02-03 11:47:16 -0300

    Criando e provendo o estado global com a Context API

--- React/dscommerce-frontend/src/App.tsx
+++ React/dscommerce-frontend/src/App.tsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import Cart from "./routes/ClientHome/Cart";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProductDetails from "./routes/ProductDetails";
-
+import { ContextCartCount } from "./utils/context-cart";
 
 export default function App() {
+  const [contextCartCount, setContextCartCount] = useState<number>(0);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientHome />}>
-          <Route index element={<Catalog/>} />
-          <Route path="catalog" element={<Catalog/>} />
-          <Route path="product-details/:productId" element={<ProductDetails/>} />
-          <Route path="cart" element={<Cart/>} />
-        </Route>
-        <Route path="*" element={<Navigate to="/"/>} />
-      </Routes>
-    </BrowserRouter>
+    <ContextCartCount.Provider
+      value={{ contextCartCount, setContextCartCount }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ClientHome />}>
+            <Route index element={<Catalog />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route
+              path="product-details/:productId"
+              element={<ProductDetails />}
+            />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </ContextCartCount.Provider>
   );
 }
---
+++ React/dscommerce-frontend/src/utils/context-cart.ts
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export type ContextCartCountType = {
+    contextCartCount: number;
+    setContextCartCount: (contextCartCount: number) => void;
+}
+
+export const ContextCartCount = createContext<ContextCartCountType>({
+    contextCartCount: 0,
+    setContextCartCount: () => {},
+
+})
