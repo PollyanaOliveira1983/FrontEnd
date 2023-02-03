e7ec388b5f25333992b46c8c3ace8a702b35af22
Pollyana <pollyana.oliveira@gmail.com>
2023-02-03 11:20:52 -0300

    Buscando os dados do localStorage

--- React/dscommerce-frontend/src/components/CartIcon/index.tsx
+++ React/dscommerce-frontend/src/components/CartIcon/index.tsx
@@ -1,11 +1,16 @@
-import cartIcon from '../../assets/cart.svg';
+import { useState } from "react";
+import cartIcon from "../../assets/cart.svg";
 import "./styles.css";
+import * as cartService from "../../services/cart-service";
 
 export default function CartIcon() {
+
+  const [cart, setCart] = useState(cartService.getCart());
+
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-      <div className="dsc-cart-count">2</div>
+      <div className="dsc-cart-count">{cart.items.length}</div>
     </>
   );
 }
