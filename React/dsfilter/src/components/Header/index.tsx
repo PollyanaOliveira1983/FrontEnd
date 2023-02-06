b49f4eb29176b9e7463d520b73b41ac01dbb1c30
Pollyana <pollyana.oliveira@gmail.com>
2023-02-06 13:38:47 -0300

    Header

---
+++ React/dsfilter/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export default function Header(){
+
+    return (
+
+        <header>
+            <div className="dr-container">
+                <h1>DS Filter</h1>
+            </div>
+        </header>
+    );
+
+}
