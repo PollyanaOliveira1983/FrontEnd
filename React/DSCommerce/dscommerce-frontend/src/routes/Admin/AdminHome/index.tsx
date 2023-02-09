bdbe12d253100758043dc4c13a3acaf32777a39f
Pollyana <pollyana.oliveira@gmail.com>
2023-02-09 11:07:25 -0300

    AdminHome

---
+++ React/DSCommerce/dscommerce-frontend/src/routes/Admin/AdminHome/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export default function AdminHome() {
+  return (
+    <main>
+      <section id="admin-home-section" className="dsc-container">
+        <h2 className="dsc-section-title dsc-mb20">
+          Bem-vindo à àrea administrativa
+        </h2>
+      </section>
+    </main>
+  );
+}
