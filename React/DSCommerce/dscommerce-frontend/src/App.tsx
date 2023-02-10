02afbecc2788bfa69916ee04228322652a313f5b
Pollyana <pollyana.oliveira@gmail.com>
2023-02-10 11:58:01 -0300

    PrivateRoute

--- React/DSCommerce/dscommerce-frontend/src/App.tsx
+++ React/DSCommerce/dscommerce-frontend/src/App.tsx
@@ -10,6 +10,7 @@ import ProductDetails from "./routes/ProductDetails";
 import { ContextCartCount } from "./utils/context-cart";
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import {history} from './utils/history';
+import { PrivateRoute } from "./components/PrivateRoute";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -30,7 +31,11 @@ export default function App() {
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
           </Route>
-          <Route path="/admin/" element={<Admin />}>
+          <Route path="/admin/" element={
+              <PrivateRoute>
+              <Admin />
+              </PrivateRoute>
+          }>
             <Route index element={<AdminHome />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
--- React/DSCommerce/dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -3,7 +3,6 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import CatalogCard from "../../../components/CatalogCard";
 import SearchBar from "../../../components/SearchBar";
 import { ProductDTO } from "../../../models/product";
-import { isAuthenticated } from "../../../services/auth-service";
 import * as productService from "../../../services/product-service";
 import "./styles.css";
 
@@ -23,8 +22,7 @@ export default function Catalog() {
   const [isLastPage, setIsLastPage] = useState(false);
 
   useEffect(() => {
-    console.log("AUTENTICADO", isAuthenticated());
-      
+
     productService
       .findPageRequest(queryParams.page, queryParams.name)
       .then((response) => {
