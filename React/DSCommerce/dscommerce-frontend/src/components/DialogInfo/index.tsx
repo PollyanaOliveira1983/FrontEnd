fc20c8f4418d1a6ec07d93660f5b1ff9a7c17df6
Pollyana <pollyana.oliveira@gmail.com>
2023-02-11 18:27:05 -0300

    Warning modal dialog layout

---
+++ React/DSCommerce/dscommerce-frontend/src/components/DialogInfo/index.tsx
@@ -0,0 +1,13 @@
+import ButtonPrimary from "../ButtonPrimary";
+
+export default function DialogInfo() {
+    return(
+        <div className="dsc-dialog-background">
+            <div className="dsc-dialog-box">
+                <h2>Sucesso!</h2>
+                <ButtonPrimary text="OK"/>
+            </div>
+        </div>
+
+    );
+}
--- React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductListing/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductListing/index.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
+import DialogInfo from "../../../components/DialogInfo";
 
 type QueryParams = {
   page: number;
@@ -101,6 +102,7 @@ export default function ProductListing() {
           </div>
         )}
       </section>
+      <DialogInfo />
     </main>
   );
 }
