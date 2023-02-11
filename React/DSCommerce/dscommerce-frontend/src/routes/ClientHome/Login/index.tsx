b99f9d4742356c37a11f9e476e767967a28d1bf5
Pollyana <pollyana.oliveira@gmail.com>
2023-02-11 20:49:22 -0300

    New format for form data

--- React/DSCommerce/dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
@@ -6,33 +6,51 @@ import { useNavigate } from "react-router-dom";
 import { ContextToken } from "../../../utils/context-token";
 
 export default function Login() {
-    
-  const {setContextTokenPayload } = useContext(ContextToken);
+  const { setContextTokenPayload } = useContext(ContextToken);
 
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState<CredentialsDTO>({
-    username: '',
-    password: '',
+  const [formData, setFormData] = useState<any>({
+    username: {
+      value: "",
+      id: "username",
+      name: "username",
+      type: "text",
+      placeholder: "Email",
+      validation: function (value: string) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          value.toLowerCase()
+        );
+      },
+      message: "Favor informar um email válido",
+    },
+    password: {
+      value: "",
+      id: "password",
+      name: "password",
+      type: "password",
+      placeholder: "Senha",
+    }
   });
 
   function handleSubmit(event: any) {
     event.preventDefault();
-    authService.loginRequest(formData)
-      .then(response => {
+    authService
+      .loginRequest({username: formData.username.value, password: formData.password.value})
+      .then((response) => {
         authService.saveAccessToken(response.data.access_token);
-        setContextTokenPayload(authService.getAccessTokenPayload())
+        setContextTokenPayload(authService.getAccessTokenPayload());
         navigate("/cart");
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Erro no login", error);
-      })
+      });
   }
 
   function handleInputChange(event: any) {
     const value = event.target.value;
     const name = event.target.name;
-    setFormData({ ...formData, [name]: value});
+    setFormData({...formData, [name]: {...formData[name], value:value}});
   }
 
   return (
@@ -45,7 +63,7 @@ export default function Login() {
               <div>
                 <input
                   name="username"
-                  value={formData.username}
+                  value={formData.username.value}
                   className="dsc-form-control"
                   type="text"
                   placeholder="Email"
@@ -56,7 +74,7 @@ export default function Login() {
               <div>
                 <input
                   name="password"
-                  value={formData.password}
+                  value={formData.password.value}
                   className="dsc-form-control"
                   type="password"
                   placeholder="Senha"
