7bc92393c1cf7c076c50723361a085e09091d3bb
Pollyana <pollyana.oliveira@gmail.com>
2023-02-13 12:02:54 -0300

    forms validate

--- React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
@@ -25,6 +25,10 @@ export default function ProductForm() {
       name: "price",
       type: "number",
       placeholder: "Preço",
+      validation: function(value : any) {
+        return Number(value) > 0;
+      },
+      message: "Favor informar um valor positivo."
     },
     imgUrl: {
       value: "",
