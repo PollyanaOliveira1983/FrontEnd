753fcd810ff5b1215219d11c4381711dfd6e5d03
Pollyana <pollyana.oliveira@gmail.com>
2023-02-13 10:41:53 -0300

    Differentiating creation and editing in the form

--- React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import FormInput from "../../../components/FormInput";
 import "./styles.css";
 import * as forms from "../../../utils/forms";
+import * as productService from "../../../services/product-service";
 
 export default function ProductForm() {
+
+  const params = useParams();
+
+  const isEditing = params.productId !== 'create';
+
   const [formData, setFormData] = useState<any>({
     name: {
       value: "",
@@ -28,6 +34,15 @@ export default function ProductForm() {
       placeholder: "Imagem",
     },
   });
+
+  useEffect(() => {
+    if(isEditing) {
+      productService.findById(Number(params.productId))
+        .then(response => {
+          console.log(response.data);
+        })
+    }
+  }, []);
 
   function handleInputChange(event: any) {
     setFormData(forms.update(formData, event.target.name, event.target.value));
