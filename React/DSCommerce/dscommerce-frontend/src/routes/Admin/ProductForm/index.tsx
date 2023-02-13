3eb0519db6343341857f1d9f45bf332e19dc1bb9
Pollyana <pollyana.oliveira@gmail.com>
2023-02-13 10:19:15 -0300

    Starting to prepare the product form

--- React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 export default function ProductForm() {
@@ -17,20 +18,14 @@ export default function ProductForm() {
                 <div>
                   <input className="dsc-form-control" type="text" placeholder="Imagem" />
                 </div>
-                <div>
-                  <select className="dsc-form-control dsc-select" required>
-                    <option value="" disabled selected>Categorias</option>
-                    <option value="1">Valor 1</option>
-                    <option value="2">Valor 2</option>
-                  </select>
-                </div>
-                <div>
-                  <textarea className="dsc-form-control dsc-textarea" placeholder="Descrição"></textarea>
-                </div>
+              
               </div>
   
               <div className="dsc-product-form-buttons">
-                <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                <Link to="/admin/products">
+                  <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                </Link>
+                
                 <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
               </div>
             </form>
--- React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductListing/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductListing/index.tsx
@@ -8,6 +8,8 @@ import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import DialogInfo from "../../../components/DialogInfo";
 import DialogConfirmation from "../../../components/DialogConfirmation";
+import ButtonInverse from "../../../components/ButtonInverse";
+import { useNavigate } from "react-router-dom";
 
 type QueryParams = {
   page: number;
@@ -15,6 +17,9 @@ type QueryParams = {
 };
 
 export default function ProductListing() {
+
+  const navigate = useNavigate();
+
   const [dialogInfoData, setdialogInfoData] = useState({
     visible: false,
     message: "Operação com sucesso!",
@@ -44,6 +49,10 @@ export default function ProductListing() {
         setIsLastPage(response.data.last);
       });
   }, [queryParams]);
+
+  function handleNewProductClick() {
+    navigate("/admin/products/create");
+  }
 
   function handleSearch(searchText: string) {
     setProducts([]);
@@ -83,13 +92,16 @@ export default function ProductListing() {
     setdialogConfirmationData({ ...dialogConfirmationData, visible: false });
   }
 
+  
   return (
     <main>
       <section id="product-listing-section" className="dsc-container">
         <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
         <div className="dsc-btn-page-container dsc-mb20">
-          <div className="dsc-btn dsc-btn-white">Novo</div>
+          <div onClick={handleNewProductClick}>
+             <ButtonInverse text="Novo"/>
+          </div>          
         </div>
 
         <SearchBar onSearch={handleSearch} />
