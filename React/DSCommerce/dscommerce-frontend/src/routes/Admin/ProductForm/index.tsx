63049a6d3f91027b799c191878ec2b75c0515b94
Pollyana <pollyana.oliveira@gmail.com>
2023-02-13 10:30:51 -0300

    Preparing the formData of the products form

--- React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
@@ -1,36 +1,82 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import FormInput from "../../../components/FormInput";
 import "./styles.css";
+import * as forms from "../../../utils/forms";
 
 export default function ProductForm() {
-    return(
-        <main>
-        <section id="product-form-section" className="dsc-container">
-          <div className="dsc-product-form-container">
-            <form className="dsc-card dsc-form">
-              <h2>Dados do produto</h2>
-              <div className="dsc-form-controls-container">
-                <div>
-                  <input className="dsc-form-control" type="text" placeholder="Nome" />
-                </div>
-                <div>
-                  <input className="dsc-form-control" type="text" placeholder="Preço" />
-                </div>
-                <div>
-                  <input className="dsc-form-control" type="text" placeholder="Imagem" />
-                </div>
-              
+  const [formData, setFormData] = useState<any>({
+    name: {
+      value: "",
+      id: "name",
+      name: "name",
+      type: "text",
+      placeholder: "Nome",
+    },
+    price: {
+      value: "",
+      id: "price",
+      name: "price",
+      type: "number",
+      placeholder: "Preço",
+    },
+    imgUrl: {
+      value: "",
+      id: "imgUrl",
+      name: "imgUrl",
+      type: "text",
+      placeholder: "Imagem",
+    },
+  });
+
+  function handleInputChange(event: any) {
+    setFormData(forms.update(formData, event.target.name, event.target.value));
+  }
+
+  return (
+    <main>
+      <section id="product-form-section" className="dsc-container">
+        <div className="dsc-product-form-container">
+          <form className="dsc-card dsc-form">
+            <h2>Dados do produto</h2>
+            <div className="dsc-form-controls-container">
+              <div>
+                <FormInput
+                  {...formData.username}
+                  className="dsc-form-control"
+                  onChange={handleInputChange}
+                />
               </div>
-  
-              <div className="dsc-product-form-buttons">
-                <Link to="/admin/products">
-                  <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
-                </Link>
-                
-                <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
+              <div>
+                <FormInput
+                  {...formData.price}
+                  className="dsc-form-control"
+                  onChange={handleInputChange}
+                />
               </div>
-            </form>
-          </div>
-        </section>
-      </main>
-    );
+              <div>
+                <FormInput
+                  {...formData.imgUrl}
+                  className="dsc-form-control"
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+
+            <div className="dsc-product-form-buttons">
+              <Link to="/admin/products">
+                <button type="reset" className="dsc-btn dsc-btn-white">
+                  Cancelar
+                </button>
+              </Link>
+
+              <button type="submit" className="dsc-btn dsc-btn-blue">
+                Salvar
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 }
