7b52dbf4ad34e763aa5ea45cefd25ede70c66e49
Pollyana <pollyana.oliveira@gmail.com>
2023-02-15 14:13:03 -0300

    atualização

--- React/DSCommerce/dscommerce-frontend/src/components/LoggedUser/index.tsx
+++ React/DSCommerce/dscommerce-frontend/src/components/LoggedUser/index.tsx
@@ -4,24 +4,21 @@ import * as authService from "../../services/auth-service";
 import { ContextToken } from "../../utils/context-token";
 
 export default function LoggedUser() {
+  const { contextTokenPayload, setContextTokenPayload } =
+    useContext(ContextToken);
 
-    const { contextTokenPayload, setContextTokenPayload } = useContext(ContextToken);
-
-    function handleLogoutClick() {
-        authService.logout();
-        setContextTokenPayload(undefined);
-    }
-        return(
-            contextTokenPayload && authService.isAuthenticated()
-            ? (
-                <div className="dsc-logged-user">
-                    <p>{contextTokenPayload.user_name}</p>
-                    <span onClick={handleLogoutClick}>Sair</span>
-                </div>
-              )  
-            :  (
-                    <Link to="/login">Entrar</Link>  
-               )
-
-    );
+  function handleLogoutClick() {
+    authService.logout();
+    setContextTokenPayload(undefined);
+  }
+  return contextTokenPayload && authService.isAuthenticated() ? (
+    <div className="dsc-logged-user">
+      <p>{contextTokenPayload.user_name}</p>
+      <span onClick={handleLogoutClick}>Sair</span>
+    </div>
+  ) : (
+    <Link to={'/login'}>
+      Entrar
+    </Link>
+  );
 }
